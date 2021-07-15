0ab3c0d7ff26c9f82940835e13b52e39dbb42605
anast <skorodinskaya01@gmail.com>
2021-07-15 17:17:19 +0300

    some changes

--- Practice2/main.js
+++ Practice2/main.js
@@ -11,6 +11,7 @@ let string = ['sky', 'sun', 'stars', 'clouds', 'rainbow'];
 console.log(string);
 
 let mix = ['dance', 'sing', 23, true, false];
+console.log(mix);
 
 // -- Створити пустий масив. Наповнити його будь-якими значеннями звертаючись до конкретного індексу. Вивести в консоль/
 let sky = [];
@@ -19,4 +20,21 @@ sky [1] = 'clouds';
 sky [2] = 'rainbow';
 console.log(sky);
 
+//- За допомогою циклу for і document.write() вивести 10 блоків div c довільним текстом всередині
+// - За допомогою циклу for і document.write() вивести 10 блоків div c довільним текстом і індексом всередині
+// - За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом всередині.
+// - За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом і індексом всередині.
+// let sport = [
+//     {kind: 'football, basketball, running'},
+//     {text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae culpa deserunt distinctio eum minima natus nesciunt\n' +
+//             '    omnis repudiandae velit voluptatum!'}
+// ];
+
+document.write(`<div>`);
+for (let i = 0; i < 10; i++) {
+    document.write(`<div>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis, impedit.</p>
+        </div>`);
+}
+document.write(`</div>`);
 
