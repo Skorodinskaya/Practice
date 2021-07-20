6ac3aec53d7fd1abbfeba8febc28dd5a15710946
Skorodinskaya <skorodinskaya01@gmail.com>
2021-07-20 21:19:31 +0300

    some changes

--- Practice4/main.js
+++ Practice4/main.js
@@ -2,7 +2,7 @@
 // створити пустий масив, наповнити його 10 об'єктами Client
 // Взяти масив (Client []).Відсортувати його по кількості товарів в полі order по зростанню. (sort)
 class Client {
-    constructor (id, name, surname , email, phone, order = 'client') {
+    constructor(id, name, surname, email, phone, order) {
         this.id = id;
         this.name = name;
         this.surname = surname;
@@ -11,16 +11,23 @@ class Client {
         this.order = order;
     }
 }
-Client = []
-const anna = new Client (1, Anna, Smith, smith.gmail, 57683, 1);
-const maxim = new Client (2, Max, Smith, smith.gmail, 57683, 4);
-const irana = new Client (3, Ira, Smith, smith.gmail, 57683, 6);
-const mira = new Client (4, Mira, Smith, smith.gmail, 57683, 5);
-const mariana = new Client (5, Maria, Smith, smith.gmail, 57683, 3);
-const ivan = new Client (6, Ivan, Smith, smith.gmail, 57683, 2);
-const serg = new Client (7, Serg, Smith, smith.gmail, 57683, 8);
-const vova = new Client (8, Vova, Smith, smith.gmail, 57683, 7);
+
+const anna = new Client(1, 'Anna', 'Smith', 'smith.gmail', 57683, [1, 'jnijn', 89]);
+const maxim = new Client(2, 'Max', 'Smith', 'smith.gmail', 57683, [4, 'jfldd', 67]);
+const irana = new Client(3, 'Ira', 'Smith', 'smith.gmail', 57683, ['ssbsd', 6]);
+const mira = new Client(4, 'Mira', 'Smith', 'smith.gmail', 57683, ['sdcds', 5]);
+const mariana = new Client(5, 'Maria', 'Smith', 'smith.gmail', 57683, [3]);
+const ivan = new Client(6, 'Ivan', 'Smith', 'smith.gmail', 57683, [2]);
+const serg = new Client(7, 'Serg', 'Smith', 'smith.gmail', 57683, ['jdf']);
+const vova = new Client(8, 'Vova', 'Smith', 'smith.gmail', 57683, [7, 'hjk']);
+
+let clients = [anna, maxim, irana, mira, mariana, ivan, serg, vova];
+
+// let sort = clients.sort( function (a, b) {
+//     return a.order.length - b.order.length
+// })
+
+let sort = clients.sort((a, b) => a.order.length - b.order.length)
 
 
-
-
+console.log(sort);
